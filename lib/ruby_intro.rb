0dfa0670ef6fcdfdea159b162512f8228197a2e0
Alic Szecsei <aszecsei@gmail.com>
2017-09-08 17:02:44 +0000

    Solved assignment

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,50 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return arr.inject(0) {|sum, i| sum+i}
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return arr.max(2).inject(0) {|sum, i| sum+i}
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return arr.combination(2).any? {|a, b| a+b==n}
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return (/[a-zA-Z&&[^aeiouAEIOU]]/ =~ s.chr) != nil
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return (/[^01]/ =~ s) == nil && (/^(?:[01]*0)?0$/ =~ s) != nil 
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    if isbn == ""
+      raise ArgumentError.new("The ISBN must not be empty")
+    end
+    if price <= 0
+      raise ArgumentError.new("The price must not be less than or equal to zero")
+    end
+    @isbn = isbn
+    @price = price
+  end
+  
+  attr_accessor :isbn
+  
+  attr_accessor :price
+  
+  def price_as_string
+    return "$" + ('%.2f' % @price)
+  end
 end
